f073334f3c8d0c51578515fd63f96698ff53c8b5
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-10-06 22:42:13 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app-layout">
+
+        <PageNavBar>
+        </PageNavBar>
+
+        <main class="container my-3">
+            <slot />
+        </main>
+
+        <PageFooterBar></PageFooterBar>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <NuxtLayout name="default">
+            <h2>Welcome to the Home Page</h2>
+            <!-- Platzhalter-Komponente einbinden -->
+            <Placeholder />
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup>
+import Placeholder from '~/components/Placeholder.vue'
+
+</script>
+
+<style scoped>
+h2 {
+    color: #007bff;
+}
+</style>
