e5cc6046119e8db1f886f13df622d6642d830c7b
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-10-15 20:30:59 +0200

    adds initial page

---
+++ components/welcome/Pitch.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="pitch" class="container">
+    Hallo, ich bin Florian Lauterbach, ein erfahrener Senior Web- und App-Entwickler. Ich spezialisiere mich darauf,
+    benutzerfreundliche und leistungsstarke digitale Lösungen für Unternehmen zu erstellen, die Ihre Online-Präsenz
+    verbessern oder eine mobile App launchen möchten. <br>
+    Mit meiner Erfahrung in der Entwicklung skalierbarer Anwendungen
+    habe ich bereits mit Automobil-Konzernen zusammen gearbeitet, aber auch lokale Unternehmen unterstützt. Wenn Sie auf
+    der
+    Suche nach einem Entwickler sind, der Ihr nächstes digitales Projekt schnell und professionell umsetzt, würde ich
+    mich freuen, wenn wir uns darüber unterhalten könnten.
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
