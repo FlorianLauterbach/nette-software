b1521065bc50e35309da5c48c1efdd69553ad81d
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-10-15 19:07:55 +0200

    adds icons/fomnts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   css: [
         '~/assets/scss/custom.scss',
   ],
+
   build: {
     transpile: ['bootstrap']
-  }
+  },
+
+  modules: ['@nuxt/icon', '@nuxt/fonts']
 })
