88bdfff9c1ec567698689a903f67da27dda0b3d0
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-10-15 19:08:33 +0200

    adds hero

---
+++ components/welcome/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero-box d-flex justify-content-center align-items-center">
+    <div class="text-overlay text-center">
+      <h1 class="display-4"><span class="gradient-text">NetteSoftware</span></h1>
+      <h2 class="lead">
+        Programmierer für maßgeschneiderte Webseiten und mobile Apps aus
+        <span class="gradient-text">Nettetal</span>
+      </h2>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.hero-box {
+  background-image: url("/images/hexagon.png");
+  background-size: cover;
+  background-position: center;
+  height: 50vh;
+  position: relative;
+  background-attachment: fixed;
+}
+
+.text-overlay {
+  z-index: 1;
+}
+
+.hero-box::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.75);
+  z-index: 0;
+}
+</style>
