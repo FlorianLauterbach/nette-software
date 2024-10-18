954cf750eab0347f61fa7714cb0d45d6ea8d8ce1
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-10-18 21:21:02 +0200

    improves layout

--- components/blogEntry/LatestPosts.vue
+++ components/blogEntry/LatestPosts.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="latest-posts">
+  <div id="latest-posts" v-if="latestPosts.length">
     <h5>Die letzten Blog Einträge</h5>
     <div class="row">
       <div v-for="post in latestPosts" :key="post.title" class="col-12 col-md-6">
-        <NuxtLink :to="`blog/${post.slug}`" class="blog-link row pb-3">
-          <div class="col-md-4">
+        <NuxtLink :to="`blog/${post.slug}`" class="card blog-link row m-3">
+          <div class="col-md-4 p-0">
             <img :src="post.image ? `/images/blog/${post.slug}/${post.image}` : '/images/logo.png'"
               class="img-fluid rounded" :alt="post.title">
           </div>
-          <div class="col-md-8">
-            <div class="card-body pl-2">
-              <h5 class="card-title gradient-text">{{ post.title }}</h5>
-              <p class="card-text">{{ post.description }}</p>
+          <div class="col-md-8 p-0 h-100">
+            <div class="card-body py-0 h-100">
+              <h5 class="card-title gradient-text mb-0">{{ post.title }}</h5>
+              <p class="card-text text-truncate-ellipsis mb-0">{{ post.description }}</p>
               <p class="card-text"><small class="text-muted">{{ post.date }}</small></p>
             </div>
           </div>
@@ -38,3 +38,25 @@ if (currentIndex === 0) {
   latestPosts.push(...blogPosts.slice(0, 4))
 }
 </script>
+
+<style>
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.text-truncate-ellipsis {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.card {
+  height: 135px;
+}
+
+.card-body {
+  max-height: 100%;
+}
+</style>
